7616d547c0ebdc942c55f7998636b02f21c62ea4
Diogo Castro <dc@diogocastro.com>
2026-07-07 21:28:40 +0100

    handleReferences

--- src/Xreferee/Lsp/Handlers/References.hs
+++ src/Xreferee/Lsp/Handlers/References.hs
@@ -2,16 +2,13 @@ module Xreferee.Lsp.Handlers.References where
 
 import ClassyPrelude hiding (Handler)
 import Control.Lens hiding (Indexable, Iso)
-import Data.IxSet.Typed ((@=))
-import Data.IxSet.Typed qualified as Ix
 import Language.LSP.Protocol.Lens qualified as LSP
 import Language.LSP.Protocol.Message qualified as LSP
 import Language.LSP.Protocol.Types qualified as LSP
 import Language.LSP.Server as LSP
-import XReferee.SearchResult qualified as X
 import Xreferee.Lsp.AppM
+import Xreferee.Lsp.Db qualified as Db
 import Xreferee.Lsp.Log qualified as Log
-import Xreferee.Lsp.Types (SymbolEntry (..), Symbols (..))
 import Xreferee.Lsp.Util qualified as Util
 
 handleReferences :: Handler AppM 'LSP.Method_TextDocumentReferences
@@ -21,13 +18,13 @@ handleReferences = \req responder -> do
   let reqUri = req ^. LSP.params ^. LSP.textDocument ^. LSP.uri
   let reqPos = req ^. LSP.params ^. LSP.position
 
-  state <- getState
+  conn <- view conn
 
-  case Util.findSymbolAtPosition reqUri reqPos state.symbols.anchors of
+  Db.findAnchorAtPosition conn reqUri reqPos >>= \case
     Nothing ->
       responder $ Right $ LSP.InR LSP.Null
-    Just anchorEntry -> do
+    Just anchor -> do
       -- Find the corresponding references
-      let ref = anchorEntry.symbol & X.getLabel & X.Reference
-      let locs = state.symbols.references @= ref & Ix.toList <&> \refEntry -> Util.symbolLocToLspLocation refEntry.loc
+      refs <- Db.findReferencesWithName conn anchor.name
+      let locs = refs <&> \ref -> Util.symbolLocToLspLocation2 ref
       responder $ Right $ LSP.InL locs
