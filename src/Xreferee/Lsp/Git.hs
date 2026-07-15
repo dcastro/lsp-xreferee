2f79598c3af42eb533fcf15bca4e81b129c56949
Diogo Castro <dc@diogocastro.com>
2026-07-16 00:09:13 +0100

    Improvement: `isBinaryFile` avoid directory traversal

--- src/Xreferee/Lsp/Git.hs
+++ src/Xreferee/Lsp/Git.hs
@@ -2,11 +2,23 @@ module Xreferee.Lsp.Git where
 
 import ClassyPrelude
 import Data.Text qualified as T
+import System.Directory qualified as Dir
 import System.Exit (ExitCode (..))
 import System.Process qualified as P
 
 -- | Check if a file is ignored by git.
 -- This is used to avoid indexing files that are not relevant to the project.
+--
+-- >>> checkIgnore "README.md"
+-- False
+-- >>> checkIgnore "src"
+-- False
+-- >>> checkIgnore "invalid-path"
+-- False
+-- >>> checkIgnore ".stack-work"
+-- True
+-- >>> checkIgnore ".stack-work/file"
+-- True
 checkIgnore :: FilePath -> IO Bool
 checkIgnore filePath = do
   -- NOTE: using `P.rawSystem` was causing vscode to tell the LSP server to shut down when opening an ignored file.
@@ -27,8 +39,10 @@ getRepoRoot = do
 {-
 
 Checks if a file is considered binary by git.
-Will return `False` if the file does not exist.
-Throws if the file is outside the current git repo.
+
+  * Will return `False` if the path is a directory.
+  * Will return `False` if the path does not exist.
+  * Throws if the file is outside the current git repo.
 
 Implementation:
 ===
@@ -62,17 +76,27 @@ The issue is that it does not match on empty files! So we'd falsely report them 
 
 >>> isBinaryFile "README.md"
 False
+>>> isBinaryFile ".stack-work"
+False
+>>> isBinaryFile "invalid-path"
+False
 
 -}
 isBinaryFile :: FilePath -> IO Bool
 isBinaryFile filePath = do
-  (exitCode, stdout, _) <- P.readProcessWithExitCode "git" ["--literal-pathspecs", "ls-files", "--eol", "--others", "--cached", "--", filePath] ""
-  -- NOTE: when the file doesn't exist, git will exit with code 0 and stdout will be empty.
-  -- So the `isInfixOf` below will return False.
-  -- This was done on purpose: we don't want to throw an exception here, the file _may_ have been deleted
-  -- after the event was triggered and before we finished processing it.
-  case exitCode of
-    ExitSuccess -> pure $ "w/-text" `T.isInfixOf` T.pack stdout
-    _ ->
-      -- This might happen if the file is outside the current repo.
-      throwIO $ userError $ "isBinaryFile: Failed to check if file is binary" <> stdout
+  -- If the path is a directory, `git ls-files` will run a deep traversal of the directory, which might be slow.
+  -- So we short-circuit here.
+  isDir <- Dir.doesDirectoryExist filePath
+  if isDir
+    then pure False
+    else do
+      (exitCode, stdout, _) <- P.readProcessWithExitCode "git" ["--literal-pathspecs", "ls-files", "--eol", "--others", "--cached", "--", filePath] ""
+      -- NOTE: when the file doesn't exist, git will exit with code 0 and stdout will be empty.
+      -- So the `isInfixOf` below will return False.
+      -- This was done on purpose: we don't want to throw an exception here, the file _may_ have been deleted
+      -- after the event was triggered and before we finished processing it.
+      case exitCode of
+        ExitSuccess -> pure $ "w/-text" `T.isInfixOf` T.pack stdout
+        _ ->
+          -- This might happen if the file is outside the current repo.
+          throwIO $ userError $ "isBinaryFile: Failed to check if file is binary" <> stdout
